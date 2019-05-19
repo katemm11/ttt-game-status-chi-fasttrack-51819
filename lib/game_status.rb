6aa895987f3bc53a696f1d9f4304c184a1b7a107
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-19 19:35:14 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -10,7 +10,8 @@ WIN_COMBINATIONS =
 
 def won?(board)
   WIN_COMBINATIONS.each do |win_combination|
-  # if position_taken?(board, win_combination[0]) && position_taken?(board, win_combination[1]) && position_taken?(board, win_combination[2]) && win_combination[0] == win_combination[1] && win_combination[1] == win_combination[2]
+  # if position_taken?(board, win_combination[0]) && position_taken?(board, win_combination[1]) && position_taken?(board, win_combination[2]) &&
+    if win_combination[0] == win_combination[1] && win_combination[1] == win_combination[2]
     return win_combination
   else
     false
