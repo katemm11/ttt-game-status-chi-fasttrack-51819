9c6ab518ab884bdbd7138157161da89394e946c4
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-19 19:17:05 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,7 +8,7 @@ WIN_COMBINATIONS =
 [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
 
-def won(board)
+def won?(board)
   WIN_COMBINATIONS.each do |i|
   if position_taken?(win_combination[0]) && position_taken?(win_combination[1]) && position_taken?(win_combination[2])
     return win_combination
