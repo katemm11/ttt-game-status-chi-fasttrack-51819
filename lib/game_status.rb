a5aeeabe31d970a3c3df07f9ce4cfbdb33027a2f
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-19 19:03:23 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,4 +5,4 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = 
-[[top_row_win = [0, 1, 2]],[middle_row_win = [3, 4, 5]], [bottom_row_win = [6, 7, 8]], [left_column_win = [0, 3, 6]], [middle_column_win = [1, 4, 7]], [right_column_win = [2, 5, 8]], [horizontal_win1 = [0, 4, 8]], [horizontal_win2 = [2, 4, 6]]]
+[[0, 1, 2]], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7],[2, 5, 8], [0, 4, 8], [2, 4, 6]]
