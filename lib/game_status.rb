00fe05e4208758aed3e8c4d6e40e1c2e3709b802
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-19 19:01:18 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,8 +7,8 @@ end
 WIN_COMBINATIONS = [
 [top_row_win = [0, 1, 2]]
 [middle_row_win = [3, 4, 5]]
-bottom_row_win = [6, 7, 8]
-left_column_win = [0, 3, 6]
+[bottom_row_win = [6, 7, 8]]
+[left_column_win = [0, 3, 6]]
 middle_column_win = [1, 4, 7]
 right_column_win = [2, 5, 8]
 horizontal_win1 = [0, 4, 8]
