f72509206f2440017ca805b7e8f4d716e4c92903
Kate Murray <katemeadowsmurray@protonmail.com>
2019-05-19 19:27:28 +0000

    Automatically backed up by Learn

--- spec/game_status_spec.rb
+++ spec/game_status_spec.rb
@@ -67,7 +67,7 @@ describe "./lib/game_status.rb" do
 
     it 'returns an array of matching indexes for a left diagonal win' do
       board = ["X", " ", "O", " ", "X", "O", " ", " ", "X"]
-
+      binding.pry
       expect(won?(board)).to match_array([0,4,8])
     end
 
